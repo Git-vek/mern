3cc51fb71eb85b65b03364aecd58ac65167a8eb9
Kumar <kumar_vivek@optum.com>
2021-01-18 15:06:16 +0530

    first cmd

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+//require('dotenv').config();
+
+
+const port = process.env.PORT || 5000;
+const usersRouter = require('./routes/userroute');
+
+const app = express();
+app.use(cors());
+app.use('/userroute', usersRouter);
+app.use(express.json());
+
+//const uri = process.env.ATLAS_URI;
+mongoose.connect('mongodb+srv://vkuma:vkuma@cluster0.ipkc7.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+).then(()=>console.log("Connection is connected")).catch(err=>console.log(err.message))
+
+
+app.listen(port, () => {
+console.log(`Server is running on port: ${port}`);
+});
