8ce235689dedd37bba82c5ee2774e2ffeaa8110f
Git-vek <47361900+Git-vek@users.noreply.github.com>
2021-01-18 15:59:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-//require('dotenv').config();
+require('dotenv').config();
 
 
 const port = process.env.PORT || 5000;
@@ -12,8 +12,8 @@ app.use(cors());
 app.use('/userroute', usersRouter);
 app.use(express.json());
 
-//const uri = process.env.ATLAS_URI;
-mongoose.connect('mongodb+srv://vkuma:vkuma@cluster0.ipkc7.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 ).then(()=>console.log("Connection is connected")).catch(err=>console.log(err.message))
 
 
